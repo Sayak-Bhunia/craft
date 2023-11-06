c840a52c6991297669eeca6d722401b94d668a41
Sayak-Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-11-06 21:59:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
 import './App.css';
 
-function App() {
+import Navbar from './Navbar';
+import HeroSection from './HeroSection';
 
-  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
-
-  console.log("Current User", user);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-      {isAuthenticated && <h3>Hhelooo {user.name}</h3>}
-      {isAuthenticated ? (
-        <button className="learn" onClick={(e) => logout()}>Logout</button>
-      ) : (
-        <button className="learn" onClick={(e) => loginWithRedirect()}>Login</button>
-      )}
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Navbar />
+    <HeroSection />
+  </>
+);
 
 export default App;
